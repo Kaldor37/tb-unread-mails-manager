e3ba7a5426103afe5b09d4dcddadaa12c9732a22
Davy <davy.gabard@gmail.com>
2018-10-08 15:23:28 +0200

    Improved build script

--- build/build.sh
+++ build/build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-OUTPUT_FILE="unreadmailsmanager.xpi"
-
-if [[ -f "$OUTPUT_FILE" ]]; then
-    rm $OUTPUT_FILE
-fi
-
 CURRENT_DIR=$(dirname $0)
 
 cd "$CURRENT_DIR/../src"
 
-zip -r unreadmailsmanager.xpi *
-mv unreadmailsmanager.xpi ../build
+VERSION=$(grep 'em:version' install.rdf | awk -F">" '{print $2}' | awk -F"<" '{print $1}')
+OUTPUT_FILE="unreadmailsmanager-${VERSION}.xpi"
+
+echo "Building extension: ${OUTPUT_FILE}"
+
+if [[ -f "../build/${OUTPUT_FILE}" ]]; then
+    rm "../build/${OUTPUT_FILE}"
+fi
+
+zip -r "${OUTPUT_FILE}" *
+mv "${OUTPUT_FILE}" ../build
 
 exit 0
 
